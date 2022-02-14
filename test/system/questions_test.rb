88257db1e83e0afe1193fdb65e4431a1c78c02fb
Nicole Ogawa <nogawa11@gmail.com>
2022-02-14 12:55:39 +0900

    added code for ask and answer questions

---
+++ test/system/questions_test.rb
@@ -0,0 +1,29 @@
+require 'application_system_test_case'
+
+class QuestionsTest < ApplicationSystemTestCase
+  # [...]
+
+  test 'saying Hello yields a grumpy response from the coach' do
+    visit ask_url
+    fill_in 'question', with: 'Hello'
+    click_on 'Ask'
+
+    assert_text 'I don\'t care, get dressed and go to work!'
+  end
+
+  test 'saying Hello? yields a grumpy response from the coach' do
+    visit ask_url
+    fill_in 'question', with: 'Hello?'
+    click_on 'Ask'
+
+    assert_text 'Silly question, get dressed and go to work!'
+  end
+
+  test 'saying I am going to work yields a good response from the coach' do
+    visit ask_url
+    fill_in 'question', with: 'I am going to work'
+    click_on 'Ask'
+
+    assert_text 'Great!'
+  end
+end
